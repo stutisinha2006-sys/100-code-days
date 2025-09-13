2d9a4ac230fa8ae9bc4d7188df08f0af78329c01
stutisinha2006-sys <stutisinha2006@gmail.com>
2025-09-13 10:12:05 +0000

    c34

---
+++ q38.c
@@ -0,0 +1,22 @@
+//Write a program to find the sum of digits of a number.
+#include <stdio.h>
+
+int main() {
+    int num, sum = 0, digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        digit = num % 10;     
+        sum += digit;         
+        num = num / 10;       
+          }
+
+    printf("Sum of digits is %d\n", sum);
+
+    return 0;
+}
+//output
+Enter a number: 48
+Sum of digits is 12
