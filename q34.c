0562ae433c8bda342e3fc5492a5c49a595e863b2
stutisinha2006-sys <stutisinha2006@gmail.com>
2025-09-13 09:55:45 +0000

    c30

---
+++ q34.c
@@ -0,0 +1,32 @@
+//Write a program to check if a number is prime
+#include <stdio.h>
+
+int main() {
+    int num, i, isPrime = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (num <= 1) {
+        isPrime = 0; 
+    } else {
+        for (i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = 0; 
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+        printf("%d is a prime number.\n", num);
+    else
+        printf("%d is not a prime number.\n", num);
+
+    return 0;
+}
+//output
+Enter a number: 2
+2 is a prime number.
+Enter a number: 6
+6 is not a prime number.
